6861b957f88abc369091116bb6e06ac3181d0863
atothfejel <adam000034@hotmail.com>
2018-11-17 19:29:54 -0800

    4th commit

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.listen(port, () => {
-  console.info('listening on %d', port);
-});
+
+
+app.listen(process.env.PORT || 5000);
